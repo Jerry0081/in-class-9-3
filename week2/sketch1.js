2e389e619c209c3dacf24656ee60817406f4946d
Jerry0081 <ty2810@nyu.edu>
2026-09-10 15:30:31 -0400

    week2
    explore more functions like draw .etc

---
+++ week2/sketch1.js
@@ -0,0 +1,24 @@
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+    background(100)
+    fill(100,0,0)
+    strokeWeight(5)
+    stroke(200,100,0)
+}
+
+function draw(){
+   
+
+}
+
+function mouseDragged(){
+    line(mouseX,mouseY,pmouseX,pmouseY)
+}
+
+function mousePressed(){
+    
+}
+
+function keyPressed(){
+    background(100)
+}
