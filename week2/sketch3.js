8e0f387fb1305d13d006438f20096f974e4cf8f4
Jerry0081 <ty2810@nyu.edu>
2026-09-16 19:05:12 -0400

    play with curve

--- week2/sketch3.js
+++ week2/sketch3.js
@@ -3,25 +3,31 @@ let y
 let xV = 5
 let yV = 5
 let diameter = 100
+let turn = 0.01
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     noStroke()
-    y = diameter / 2
-    x = width / 2
+    y = diameter/2
+    x = width/2
 }
 
 function draw() {
 
-    let r = map(y, diameter / 2, height - diameter / 2, 0, 255)
-    let b = map(x,diameter / 2, height - diameter / 2, 0, 255)
+    let r = map(y, diameter/2, height - diameter/2, 0, 255)
+    let b = map(x,diameter/2, height - diameter/2, 0, 255)
    
     fill(r, 0, b)
     ellipse(x, y, diameter)
-    x+=xV
-    y+=yV
+    
+    let curveXV = xV*cos(turn)-yV*sin(turn)
+    let curveYV = xV*sin(turn)+yV*cos(turn)
+    xV = curveXV
+    yV = curveYV
+    x += curveXV
+    y += curveYV
 
-    if (y > height - diameter/2){
+    if (y > height - diameter/2) {
         yV = -yV
     }
     if (y < diameter/2){
