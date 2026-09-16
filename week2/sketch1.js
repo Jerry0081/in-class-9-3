c05987e361f05350e2d6f598321b3bd7388148a6
Jerry0081 <ty2810@nyu.edu>
2026-09-16 18:01:44 -0400

    add a color change

--- week2/sketch1.js
+++ week2/sketch1.js
@@ -1,24 +1,31 @@
 function setup(){
     createCanvas(windowWidth,windowHeight)
-    background(100)
+    background(255)
     fill(100,0,0)
     strokeWeight(5)
     stroke(200,100,0)
+    let c
 }
 
-function draw(){
-   
-
-}
 
 function mouseDragged(){
-    line(mouseX,mouseY,pmouseX,pmouseY)
+    let blue = color(0, 0, 200)
+    let green = color(0, 200, 0)
+    let red = color(200, 0, 0)
+     if(mouseY < height / 2){
+        let amount = map(mouseY, 0, height / 2, 0, 1)
+        c = lerpColor(blue, green, amount)
+    } else {
+        let amount = map(mouseY, height / 2, height, 0, 1)
+        c = lerpColor(green, red, amount)
+    }
+
+    stroke(c)
+    line(mouseX, mouseY, pmouseX, pmouseY)
 }
 
-function mousePressed(){
-    
-}
+
 
 function keyPressed(){
-    background(100)
+    background(255)
 }
--- week2/sketch3.js
+++ week2/sketch3.js
@@ -1,32 +1,40 @@
-  let x,y;
-    let diameter = 100
-    let yV=5,xV=3
-function setup(){
-    createCanvas(windowWidth,windowHeight)
-   
-    y=diameter/2
-    x=width/2
+let x;
+let y;
+let xV = 5;
+let yV = 5;
+let diameter = 100;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    noStroke()
+    y = diameter / 2
+    x = width / 2
 }
 
-function draw(){
-    background(100)
-    fill(100,50,50)
-    noStroke()
-    ellipse(x,y,diameter)
-    //x+=3
-    x+=xV
-    y+=yV
+function draw() {
 
-    if(y>height-diameter/2){
-        yV = -yV;
+    let r = map(y, diameter / 2, height - diameter / 2, 0, 255)
+    let b = map(x,diameter / 2, height - diameter / 2, 0, 255)
+   
+    fill(r, 0, b)
+    ellipse(x, y, diameter)
+    x += xV
+    y += yV
+
+    if (y > height - diameter/2) {
+        yV = -yV
     }
-    if(y<diameter/2){
-        yV = -yV;
+
+  
+    if (y < diameter/2) {
+        yV = -yV
     }
-    if(x>width-diameter/2){
-        xV = -xV;
+
+    if (x > width - diameter/2) {
+        xV = -xV
     }
-    if(x<diameter/2){
-        xV = -xV;
+
+    if (x < diameter/2) {
+        xV = -xV
     }
 }
